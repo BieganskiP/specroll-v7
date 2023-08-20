5363358a6fa6a800590d3aecf0a1ed20455f0fe1
BieganskiP <bieganski1996@gmail.com>
2023-08-20 23:14:37 +0200

    bug fix

--- src/components/ProductCard/ProductCard.tsx
+++ src/components/ProductCard/ProductCard.tsx
@@ -15,9 +15,11 @@ const ProductCard: React.FC<ProductCardProps> = ({
   url,
 }) => {
   return (
-    <div className="rounded-md shadow-md w-full overflow-hidden flex my-4 bg-gray-100">
-      <Image src={url} alt={productName} width={500} height={500} />
-      <div className="p-4">
+    <div className="rounded-md shadow-md w-full overflow-hidden my-4 bg-gray-100 flex flex-col md:flex-row">
+      <div className="w-full md:w-1/2">
+        <Image src={url} alt={productName} width={500} height={500} />
+      </div>
+      <div className="w-full md:w-1/2 p-4">
         <h3 className="font-bold text-xl mb-4">{productName}</h3>
         <p className="text-gray-700">{description}</p>
       </div>
